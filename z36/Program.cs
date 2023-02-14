cb9ac251b7eaee9652d547d1ef30449c06c4c13e
SergeyK <it-dgstars@mail.ru>
2023-02-14 15:30:08 +0300

    Релиши задачу 36

---
+++ z36/Program.cs
@@ -0,0 +1,19 @@
+﻿int [] newarray()
+{
+    int [] array = new int[4];
+    int sum = 0;
+    for (int i=0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,100);
+        Console.Write($"{array[i]} ");
+        if(i % 2 != 0)
+        {
+           sum = sum + array[i]; 
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Сумма элеменетов с нечетными индексами: {sum}");
+    return array;
+}    
+
+newarray ();
