e075e05d633c9325807f5cca451534a4822e11f9
SergeyK <it-dgstars@mail.ru>
2023-02-14 15:31:05 +0300

    Релиши задачу 38

---
+++ z38/Program.cs
@@ -0,0 +1,23 @@
+﻿double [] newarray()
+{
+    double max = 0;
+    double [] array = {3.0, 7.1, 22.5, 2.7, 78.8};
+    double min = array[0];
+    for (int i = 0; i <array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    double res = max - min;
+    Console.WriteLine(res);
+    return array;
+}
+
+
+newarray();
