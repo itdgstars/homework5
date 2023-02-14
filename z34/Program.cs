81b252306edef53bcabfe23e704bc8b99adf6fc1
SergeyK <it-dgstars@mail.ru>
2023-02-14 15:28:53 +0300

    Релиши задачу 34

---
+++ z34/Program.cs
@@ -0,0 +1,21 @@
+﻿int [] newarray()
+{
+    int [] array = new int[4];
+    int sum = 0;
+    for (int i=0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+        Console.Write($"{array[i]} ");
+        if (array[i]%2 == 0)
+        {
+            sum++;
+        }
+        
+    }
+    Console.WriteLine();
+    Console.WriteLine ("Количество четных чисел в массиве: " + sum);
+    return array;
+    
+}
+
+newarray();
